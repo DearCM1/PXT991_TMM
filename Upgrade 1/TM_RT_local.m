1ab00b192bf801a20d4cad87c788f6199414eb33
DearCM1 <calumdear@gmail.com>
2020-12-14 18:16:39 +0000

    added data exporting capabilities

--- Upgrade 1/TM_RT_local.m
+++ Upgrade 1/TM_RT_local.m
@@ -1,40 +1,46 @@
-% Last Updated; 2020/11/28 20:18; Calum
+% This code was last updated to check the results in comparision to the
+% those obtained for a thin dielectric film in Born, Optics, page 64.
+
+% Last Updated; 2020/12/02 20:18; Calum
 % added generalisations to AB slab multipications for N=0.5,1.0,1.5,2.0...
 % ======================================================== PARAMETERS === %
 % first and last p_values in the TE polarisation
 theta_1 = 0.0;
+theta_2 = 0.0; %normal incidence
 n_f = 1.00;
 n_l = 1.50;
 p_f = p(n_f, theta_1);
 p_l = p(n_l, theta_1);
 
 % arguments of the stratisfied media
-n_1 = 2.12;
-n_2 = 2.44;
-d_1 = 53.10e-9;
-d_2 = 46.10e-9;
+n_1 = 1.45;
+n_2 = 2.30;
+d_1 = 120.7e-9;
+d_2 = 76.1e-9;
 p_1 = p(n_1, theta_1);
 p_2 = p(n_2, theta_1);
-N = 20.5;  %N periods of stratified media
+N = 12;  %N periods of stratified media
 
 % value of the PBG centre frequency
-lambda = 476e-9;
-w_0 = w_calc(lambda);
+c = 2.99792458e+8;
+D = d_1 + d_2;
+n_avg = (d_1*n_1+d_2*n_2)/D;
+w_0 = pi*c/(n_avg*D);
 
-%range of frequencies
-w_min = w_0*0.5;
-w_max = w_0*1.5;
+% range of frequencies
+w_min = 0.5*w_0;
+w_max = 1.5*w_0;
 
 % ========================================================== TMM LOOP === %
 %for loop to find R,T as a function of frequency
-w_in = w_min:0.005*w_0:w_max;
+w_in = w_min:0.001*w_0:w_max;
 T_out = zeros([1,length(w_in)]);
 R_out = zeros([1,length(w_in)]);
 for i = 1:length(w_in)
     w = w_in(i);
     M_A = TM(n_1, d_1, w, p_1);
     M_B = TM(n_2, d_2, w, p_2);
-    %initialise and multiply the matrix layers N times
+    %initialise and multiply the matrix AB layers N times
     M_N = [1 1;1 1];
     for j = 1:N*2
         if j == 1
@@ -58,36 +64,30 @@ for i = 1:length(w_in)
     T_out(i) = abs(t).^2*p_l/p_f;
 end
 
+% ========================================================== PLOTTING === %
+% plot of R and log(T) vs w/w_0
+rel_w = w_in/w_0;
+figure(3)
+plot(rel_w,R_out)
+xlabel('w')
+ylabel('R')
+figure(2)
+semilogy(rel_w,T_out)
+xlabel('w/w_0')
+ylabel('T')
+
 % ============================================================ EXPORT === %
-fileID = fopen('RT2B.txt','w');
-fprintf(fileID,'%6s %12s %18s\n','lam','R','T');
-fprintf(fileID,'%6.1f %12.4f %12.4f\r\n',[wav(w_in);R_out;T_out]);
+fileID = fopen('R_i.txt','w');
+fprintf(fileID,'%6s %12s\n','R','w/w0');
+fprintf(fileID,'%6.5f %12.5f\n',[R_out;rel_w]);
+fclose(fileID);
+fileID = fopen('T_i.txt','w');
+fprintf(fileID,'%6s %12s\n','T','w/w0');
+fprintf(fileID,'%6.5f %12.5f\n',[T_out;rel_w]);
 fclose(fileID);
 
-% ========================================================== PLOTTING === %
-% R graph as a function of wavelength
-figure(1)
-plot(w_calc(w_in),R_out)
-xlim([400,525])
-ylim([0,1])
-xlabel('Wavelength, lambda (nm)')
-ylabel('R')
-
-% T not in use
-%figure(2)
-%semilogy(rel_w,T_out)
-%xlabel('w/w_0')
-%ylabel('T')
-
-
 % ========================================================= FUNCTIONS === %
-% angular frequency calculator
-function ang_freq = w_calc(lambda)
-    c = 2.99792458e+8;
-    ang_freq = 2 * pi * c / lambda;
-end
-
-% p_value calculator
+% p_value
 function p_val = p(n, theta)
     p_val = n * cos(theta);
 end
